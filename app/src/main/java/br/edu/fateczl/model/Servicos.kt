83fb8f1ac1e340ab45faf9b610809c8babdb29d0
Wallace Martins <wmvwallace@gmail.com>
2024-07-18 15:38:28 -0300

    Home Page

---
+++ app/src/main/java/br/edu/fateczl/model/Servicos.kt
@@ -0,0 +1,6 @@
+package br.edu.fateczl.model
+
+data class Servicos(
+    val img: Int? = null,
+    val nome: String? = null
+)
--- app/src/main/java/br/edu/fateczl/view/Home.kt
+++ app/src/main/java/br/edu/fateczl/view/Home.kt
@@ -1,21 +1,44 @@
 package br.edu.fateczl.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import br.edu.fateczl.R
+import br.edu.fateczl.adapter.ServicosAdapter
+import br.edu.fateczl.databinding.ActivityHomeBinding
+import br.edu.fateczl.model.Servicos
 
 class Home : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var servicosAdapter: ServicosAdapter
+    private val listaServicos: MutableList<Servicos> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        val nome = intent.extras?.getString("nome")
+
+        binding.nomeUsuario.text = "Bem-vindo(a), $nome"
+        val recyclerViewServicos = binding.recyclerViewServices
+        recyclerViewServicos.layoutManager = GridLayoutManager(this, 2)
+        servicosAdapter = ServicosAdapter(this, listaServicos)
+        recyclerViewServicos.setHasFixedSize(true)
+        recyclerViewServicos.adapter = servicosAdapter
+        getServicos()
+    }
+
+    private fun getServicos() {
+        val servico1 = Servicos(R.drawable.img1, "Corte de cabelo")
+        listaServicos.add(servico1)
+        val servico2 = Servicos(R.drawable.img2, "Corte de barba")
+        listaServicos.add(servico2)
+        val servico3 = Servicos(R.drawable.img3, "Lavagem de cabelo")
+        listaServicos.add(servico3)
+        val servico4 = Servicos(R.drawable.img4, "Tratamento de cabelo")
+        listaServicos.add(servico4)
     }
 }
